a64341405312e539f07cd61b11dc901fe6fd4b78
Chase Armstrong <chasejarms@gmail.com>
2017-05-16 20:28:07 -0700

    basic typescript structure

---
+++ typescript_practice/greeter.js
@@ -0,0 +1,5 @@
+var greeter = function (person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+};
+var user = { firstName: "Jane", lastName: "User" };
+document.body.innerHTML = greeter(user);
