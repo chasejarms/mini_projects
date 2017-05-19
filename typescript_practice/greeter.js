d31976083f24ad6bc68345e6f0aafcc3303e1f43
Chase Armstrong <chasejarms@gmail.com>
2017-05-18 18:16:44 -0700

    Set up angular app

--- typescript_practice/greeter.js
+++ typescript_practice/greeter.js
@@ -1,5 +1,15 @@
+// using classes
+var Student = (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+    return Student;
+}());
 var greeter = function (person) {
     return "Hello, " + person.firstName + " " + person.lastName;
 };
-var user = { firstName: "Jane", lastName: "User" };
+var user = new Student("Jane", "M.", "User");
 document.body.innerHTML = greeter(user);
